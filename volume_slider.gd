cd43f01743d67a77e3a6c921c23c9f205fb9ac63
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-10-03 23:49:52 +0200

    fix volume being super loud on launch

--- volume_slider.gd
+++ volume_slider.gd
@@ -2,7 +2,7 @@ extends HSlider
 
 func _ready() -> void:
 	value = config.config.get_value("options","volume")
-	AudioServer.set_bus_volume_linear(0,value)
+	AudioServer.set_bus_volume_linear(0,value/50.0)
 
 func _value_changed(new_value: float) -> void:
 	config.config.set_value("options","volume",new_value)
