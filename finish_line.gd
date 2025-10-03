1dcce77c3eecbd5c359bd00a25e1f43eb47beb33
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-10-04 00:19:41 +0200

    make levels require previous to be completed to unlock

--- finish_line.gd
+++ finish_line.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
-var should_load_next = false
+var should_return_to_menu = false
 
 func _on_area_2d_body_entered(_body: Node2D) -> void:
 	# because loading the scene here causes a crash,
 	# since "removing a collisionobject node during a physics callback is not allowed"
-	should_load_next = true
+	should_return_to_menu = true
 
-func _process(delta: float) -> void:
-	if should_load_next:
+func _process(_delta: float) -> void:
+	if should_return_to_menu:
+		var level_name = get_parent().get_parent().get_child(0).name.trim_prefix("Level")
+		config.config.set_value("levels",level_name,"completed")
+		config.save()
 		get_parent().get_parent().return_to_menu()
--- level_button.gd
+++ level_button.gd
@@ -2,6 +2,18 @@ extends Button
 
 @onready var active_level = self.get_node("../../../ActiveLevel")
 
+func reload_state():
+	if name == "1":
+		return
+	var prev_level = str(int(name) - 1)
+	var value = config.config.get_value("levels",prev_level,"no")
+	disabled = value != "completed"
+		
+
+func _ready() -> void:
+	reload_state()
+	visibility_changed.connect(reload_state)
+
 func _pressed() -> void:
 	var level_name = "level_" + name
 	var path = "res://" + level_name + ".tscn"
