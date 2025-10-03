e013626e7d22657b4ed972a87b27776593c9773a
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-10-03 23:52:20 +0200

    make test audio play when volume is changed in settings

--- volume_slider.gd
+++ volume_slider.gd
@@ -4,7 +4,8 @@ func _ready() -> void:
 	value = config.config.get_value("options","volume")
 	AudioServer.set_bus_volume_linear(0,value/50.0)
 
-func _value_changed(new_value: float) -> void:
-	config.config.set_value("options","volume",new_value)
-	AudioServer.set_bus_volume_linear(0,new_value/50.0)
+func _on_drag_ended(_value_changed: bool) -> void:
+	config.config.set_value("options","volume",value)
+	AudioServer.set_bus_volume_linear(0,value/50.0)
+	$AudioStreamPlayer.play()
 	config.save()
